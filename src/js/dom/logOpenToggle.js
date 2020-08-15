69b643bff6aa2441d319f18fc040c538d327d1b9
xtrp <36894883+xtrp@users.noreply.github.com>
2020-08-15 00:38:52 -0400

    Style Show/Hide log button

--- src/js/dom/logOpenToggle.js
+++ src/js/dom/logOpenToggle.js
@@ -1,10 +1,11 @@
 const toggleLogBtn = document.querySelector('.logo-buttons .toggle-log');
 const closeLogBtn = document.querySelector('.log .close-log');
 
-const toggleLogOpen = () => {
+const toggleLogOpen = (e) => {
   document.querySelector('.log').classList.toggle('open');
   toggleLogBtn.classList.toggle('log-open');
   document.body.classList.toggle('log-open');
 };
+toggleLogBtn.addEventListener('focus', (e) => e.target.blur());
 toggleLogBtn.addEventListener('click', toggleLogOpen);
 closeLogBtn.addEventListener('click', toggleLogOpen);
